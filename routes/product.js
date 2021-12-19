a037103c3462864de2a08c3be3ce2b2599a793ac
Alfonso Graziano <alfonso.lokk3d@gmail.com>
2021-12-19 11:14:28 +0100

    Product fix

--- routes/product.js
+++ routes/product.js
@@ -4,21 +4,21 @@ const { check } = require('express-validator')
 const {
     checkToken,
     restrictTo
-} = require("../middleware/middleware")
+} = require("simple-jwt-auth-protocol")
 const UserRoles = require("../config/userRoles")
 
 
-router.get("/", checkToken, restrictTo([UserRoles.SHOP_MANAGER]), productController.get)
+router.get("/", checkToken(), restrictTo([UserRoles.SHOP_MANAGER]), productController.get)
 
 
 router.post("/add", [
     check("name").isLength({ min: 2 }),
     check("price").isNumeric(),
-], checkToken, restrictTo([UserRoles.SHOP_MANAGER]), productController.add)
+], checkToken(), restrictTo([UserRoles.SHOP_MANAGER]), productController.add)
 
-router.delete("/delete",  checkToken, restrictTo([UserRoles.SHOP_MANAGER]), productController.deleteProduct)
+router.delete("/delete",  checkToken(), restrictTo([UserRoles.SHOP_MANAGER]), productController.deleteProduct)
 
-router.delete("/update",  checkToken, restrictTo([UserRoles.SHOP_MANAGER]), productController.update)
+router.delete("/update",  checkToken(), restrictTo([UserRoles.SHOP_MANAGER]), productController.update)
 
 
 
